80bdef4a7efd4aca4ba440e15e2b41469cb66017
Aiden Cullo <aidencullo@gmail.com>
2024-07-31 15:43:23 -0400

    ignore dist

--- dist/routes/burritoRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const burritoController_1 = require("../controllers/burritoController");
-const router = (0, express_1.Router)();
-router.get('/burrito', burritoController_1.getBurritos);
-exports.default = router;
